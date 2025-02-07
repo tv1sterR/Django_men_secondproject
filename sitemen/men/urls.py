be08e33f6b8447ac2048a4467f3dcd1bda496ab0
александр карпенко <karpenko2702@gmail.com>
2025-02-07 15:12:08 +0300

    Commit

--- sitemen/men/urls.py
+++ sitemen/men/urls.py
@@ -6,7 +6,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<slug:cat_slug>/', views.categories),
-    path("archive/<year4:year>/", views.archive)
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/<slug:cat_slug>/', views.categories, name='cats'),
+    path("archive/<year4:year>/", views.archive, name='archive')
 ]
--- sitemen/men/views.py
+++ sitemen/men/views.py
@@ -1,8 +1,33 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.template.context_processors import request
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
 
 def index(request):
-    return HttpResponse("Страница приложения men")
+    # t = render_to_string('men/index.html')
+    # return HttpResponse(t)
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 27.56,
+        'lst': [1,2, 'abc', True],
+        'set': {1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'men/index.html', context=data)
+
+def about(request):
+    return render(request, 'men/about.html', {'title': 'О сайте'})
 
 def categories(request, cat_slug):
     if request.POST:
@@ -11,7 +36,8 @@ def categories(request, cat_slug):
 
 def archive(request, year):
     if year > 2025:
-        raise Http404()
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
     
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
